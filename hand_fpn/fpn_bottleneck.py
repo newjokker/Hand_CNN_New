0f0a18764f93e08aacd673dff97f5c62dc8888f6
jokker <18761609908@163.com>
2021-09-22 13:42:21 +0800

    * fpn

--- hand_fpn/fpn_bottleneck.py
+++ hand_fpn/fpn_bottleneck.py
@@ -4,13 +4,14 @@
 import torch.nn as nn
 import torch.nn.functional as F
 import math
+from torchsummary import summary
 
 
 class Bottleneck(nn.Module):
     expansion = 4
     def __init__(self, in_planes, planes, stride=1, downsample=None):
         super(Bottleneck, self).__init__()
-
+        # conv, BN, Relu | conv, BN, Relu | conv, BN
         self.bottle_neck = nn.Sequential(
             nn.Conv2d(in_planes, planes, 1, bias=False),
             nn.BatchNorm2d(planes),
@@ -21,7 +22,6 @@ class Bottleneck(nn.Module):
             nn.Conv2d(planes, self.expansion * planes, 1, bias=False),
             nn.BatchNorm2d(self.expansion * planes),
         )
-
         self.relu = nn.ReLU(inplace=True)
         self.downsample = downsample
 
@@ -33,6 +33,95 @@ class Bottleneck(nn.Module):
         out += identify
         out = self.relu(out)
         return out
+
+
+class FPN(nn.Module):
+
+    def __init__(self, layers):
+        super(FPN, self).__init__()
+        self.inplanes = 64
+        self.conv1 = nn.Conv2d(3, 64, 7, 2, 3, bias=False)
+        #
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(3, 2, 1)
+        #
+        self.layer1 = self._make_layer(64, layers[0])
+        self.layer2 = self._make_layer(128, layers[1], 2)
+        self.layer3 = self._make_layer(256, layers[2], 2)
+        self.layer4 = self._make_layer(512, layers[3], 2)
+        #
+        self.toplayer = nn.Conv2d(2048, 256, 1, 1, 0)
+        #
+        self.smooth1 = nn.Conv2d(256, 256, 3, 1, 1)
+        self.smooth2 = nn.Conv2d(256, 256, 3, 1, 1)
+        self.smooth3 = nn.Conv2d(256, 256, 3, 1, 1)
+        # 横向连接，保证通道数相同
+        self.latlayer1 = nn.Conv2d(1024, 256, 1, 1, 0)
+        self.latlayer2 = nn.Conv2d(512, 256, 1, 1, 0)
+        self.latlayer3 = nn.Conv2d(256, 256, 1, 1, 0)
+
+    def _make_layer(self, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != Bottleneck.expansion * planes:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, Bottleneck.expansion * planes, 1, stride, bias=False),
+                nn.BatchNorm2d(Bottleneck.expansion * planes)
+            )
+        layers = [Bottleneck(self.inplanes, planes, stride, downsample)]
+        self.inplanes = planes * Bottleneck.expansion
+        for i in range(1, blocks):
+            layers.append(Bottleneck(self.inplanes, planes))
+        return nn.Sequential(*layers)
+
+    def _up_sample_add(self, x, y):
+        _, _, K, W = y.shape
+        return F.upsample(x, size=(H, W), mode='bilinear') + y
+
+    def forward(self):
+        c1 = self.maxpool(self.relu(self.bn1(self.conv1(x))))
+        c2 = self.layer1(c1)
+        c3 = self.layer2(c2)
+        c4 = self.layer3(c3)
+        c5 = self.layer4(c4)
+        #
+        p5 = self.toplayer(c5)
+        p4 = self._up_sample_add(p5, self.latlayer1(c4))
+        p3 = self._up_sample_add(p4, self.latlayer2(c3))
+        p2 = self._up_sample_add(p3, self.latlayer3(c2))
+        #
+        p4 = self.smooth1(p4)
+        p3 = self.smooth1(p3)
+        p2 = self.smooth1(p2)
+        return p2, p3, p4, p5
+
+
+if __name__ == "__main__":
+
+    model = FPN([3,4,6,3])
+    summary(model)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
